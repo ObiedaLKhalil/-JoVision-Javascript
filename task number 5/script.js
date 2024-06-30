ed01421e94c21f61a3fcb565e35c84284b813c5d
ObiedaLKhalil <olkhalil2024@gmail.com>
2024-06-30 12:51:57 +0300

    end task 5

---
+++ task number 5/script.js
@@ -0,0 +1,26 @@
+
+function clickMe() {
+var numbers = [];
+for (var i = 0; i <= 100; i++) {
+    numbers.push(i);
+
+}
+document.getElementById("array").innerHTML = numbers;
+
+
+    var sum = 0;
+
+     for (var j = 0; j < numbers.length; j++) {
+if(numbers[j]%2==0)
+        sum += numbers[j];
+    }
+
+      console.log(sum);
+
+document.getElementById("sum").innerHTML = sum;
+
+sum=0;
+}
+
+var button = document.getElementById("clickButton");
+button.addEventListener("click", clickMe);
